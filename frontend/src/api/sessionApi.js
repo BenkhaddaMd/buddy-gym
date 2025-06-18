f37b528a6ac0b14877b2f0a0382818ef02d4ff0f
benahmed.meddhia <benahmed.meddhia@gmail.com>
2025-06-18 16:30:06 +0200

    create a session with sport entity

---
+++ frontend/src/api/sessionApi.js
@@ -0,0 +1,35 @@
+import axiosInstance from "./axiosInstance";
+
+// Auth API Service
+const sessionService = {
+ 
+  getSportsList: async () => {
+    try {
+      const response = await axiosInstance.get("/account/sports/");
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+  // Créer une nouvelle session
+  createSession: async (sessionData) => {
+    try {
+      const formData = new FormData();
+      formData.append("sport", sessionData.sport);
+      formData.append("location", sessionData.location);
+      formData.append("date", sessionData.date);
+      formData.append("time", sessionData.time);
+      formData.append("max_participants", sessionData.max_participants);
+
+      const response = await axiosInstance.post("/account/sessions/", formData);
+
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+};
+
+export default sessionService;
