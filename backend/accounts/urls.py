8b1268919e8e032c451aa1c1938850b717808961
benkhada111 <benkhada111@gmail.com>
2025-06-18 16:18:38 +0200

    adding participations model and views

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import JoinSessionView, MatchUsersView, SessionListCreateView, SportPreferenceView, UserAvailabilityView, SportListView, UserUpdateView
+from .views import MatchUsersView, MyParticipationsView, ParticipateSessionView, SessionListCreateView, SportPreferenceView, UserAvailabilityView, SportListView, UserUpdateView
 
 
 urlpatterns = [
     path('user/update/', UserUpdateView.as_view(), name='custom-user-update'),
     path('sessions/', SessionListCreateView.as_view(), name='session-list-create'),
-    path('sessions/<int:pk>/join/', JoinSessionView.as_view(), name='join-session'),
     path('availability/', UserAvailabilityView.as_view(), name='user-availability'),
     path('preference/', SportPreferenceView.as_view(), name='sport-preference'),
     path('matches/', MatchUsersView.as_view(), name='user-matches'),
     path('sports/', SportListView.as_view(), name='sport-list'),
+    path('sessions/<int:session_id>/join/', ParticipateSessionView.as_view(), name='participate-session'),
+    path('my-participations/', MyParticipationsView.as_view(), name='my-participations'),
 ]
